217a7409d8cc105ff384df6f75d2c2e0157b4fc6
Manuel Vera <manuelvera202@gmail.com>
2020-10-23 13:15:05 -0300

    algoritm for fibonacci number
    first exercise of week 2

--- week_one/Maximum_Pairwise_Product/max_pair_product_fast.py
+++ week_one/Maximum_Pairwise_Product/max_pair_product_fast.py
@@ -1,20 +1,21 @@
-n = int(input())
-a = [int(x) for x in input().split()]
+def max_pair(a , n):
+    index1 = 0
+    for i in range(n):
+        if a[i] >= a[index1]:
+            index1 = i
+    if index1 == 0:
+        index2 = 1
+    else:
+        index2 = 0
 
+    for i in range(n):
+        if i != index1 and a[i] > a[index2]:
+            index2 = i
 
-index1 = 0
-for i in range(n):
-    if a[i] >= a[index1]:
-        index1 = i
-if index1 == 0:
-    index2 = 1
-else:
-    index2 = 0
+    result = a[index1] * a[index2]
+    return result
 
-for i in range(n):
-    if i != index1 and a[i] > a[index2]:
-        index2 = i
-
-result = a[index1] * a[index2]
-
-print(result)
+if __name__ == '__main__':
+    n = int(input())
+    a = [int(x) for x in input().split()]
+    print(max_pair(a, n))
