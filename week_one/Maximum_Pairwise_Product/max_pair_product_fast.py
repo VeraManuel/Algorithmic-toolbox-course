223847c6424e68dce1dbee897e4c2807c9e1d7f4
Manuel Vera <manuelvera202@gmail.com>
2020-10-23 13:18:14 -0300

    Revert "algoritm for fibonacci number"
    This reverts commit 217a7409d8cc105ff384df6f75d2c2e0157b4fc6.

--- week_one/Maximum_Pairwise_Product/max_pair_product_fast.py
+++ week_one/Maximum_Pairwise_Product/max_pair_product_fast.py
@@ -1,21 +1,20 @@
-def max_pair(a , n):
-    index1 = 0
-    for i in range(n):
-        if a[i] >= a[index1]:
-            index1 = i
-    if index1 == 0:
-        index2 = 1
-    else:
-        index2 = 0
+n = int(input())
+a = [int(x) for x in input().split()]
 
-    for i in range(n):
-        if i != index1 and a[i] > a[index2]:
-            index2 = i
 
-    result = a[index1] * a[index2]
-    return result
+index1 = 0
+for i in range(n):
+    if a[i] >= a[index1]:
+        index1 = i
+if index1 == 0:
+    index2 = 1
+else:
+    index2 = 0
 
-if __name__ == '__main__':
-    n = int(input())
-    a = [int(x) for x in input().split()]
-    print(max_pair(a, n))
+for i in range(n):
+    if i != index1 and a[i] > a[index2]:
+        index2 = i
+
+result = a[index1] * a[index2]
+
+print(result)
